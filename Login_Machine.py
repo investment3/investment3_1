7f6b418c3c2cef8bdd153c47bb302ba3d199d2a1
mini <maya2001bee@naver.com>
2022-11-28 13:54:58 +0900

    주문 전송하기

--- Login_Machine.py
+++ Login_Machine.py
@@ -1,12 +1,12 @@
 import sys                        # system specific parameters and functions : 파이썬 스크립트 관리
 from PyQt5.QtWidgets import *     # GUI의 그래픽적 요소를 제어       하단의 terminal 선택, activate py37_32,  pip install pyqt5,   전부다 y
 from PyQt5 import uic             # ui 파일을 가져오기위한 함수
-
 from PyQt5.QtCore import *        # eventloop/스레드를 사용 할 수 있는 함수 가져옴.
 
 ################# 부가 기능 수행(일꾼) #####################################
 from kiwoom import Kiwoom          # 키움증권 함수/공용 방 (싱글턴)
 from Qthread_1 import Thread1      # 계좌평가잔고내역 가져오기
+from Qthread_2 import Thread2      # 계좌 관리
 
 #=================== 프로그램 실행 프로그램 =========================#
 
@@ -28,6 +28,7 @@ class Login_Machnine(QMainWindow, QWidget, form_class):       # QMainWindow : Py
         self.label_14.setText(str("총평가손익금액"))
         self.label_15.setText(str("총수익률(%)"))
 
+
         #### 기타 함수
         self.login_event_loop = QEventLoop()  # 이때 QEventLoop()는 block 기능을 가지고 있다.
 
@@ -36,8 +37,10 @@ class Login_Machnine(QMainWindow, QWidget, form_class):       # QMainWindow : Py
         self.set_signal_slot()                # 키움로그인을 위한 명령어 전송 시 받는 공간을 미리 생성한다.
         self.signal_login_commConnect()
 
-        ####이벤트 생성 및 진행
-        self.call_account.clicked.connect(self.c_acc)
+        #####이벤트 생성 및 진행
+        self.call_account.clicked.connect(self.c_acc)         # 계좌정보가져오기
+        self.acc_manage.clicked.connect(self.a_manage)         # 계좌정보가져오기
+
 
     def setUI(self):
         self.setupUi(self)                # UI 초기값 셋업
@@ -53,7 +56,7 @@ class Login_Machnine(QMainWindow, QWidget, form_class):       # QMainWindow : Py
         if errCode == 0:
             print("로그인 성공")
             self.statusbar.showMessage("로그인 성공")
-            self.get_account_info()  # 로그인시 계좌정보 가져오기
+            self.get_account_info()                    # 로그인시 계좌정보 가져오기
 
         elif errCode == 100:
             print("사용자 정보교환 실패")
@@ -74,6 +77,12 @@ class Login_Machnine(QMainWindow, QWidget, form_class):       # QMainWindow : Py
         ##### 1번 일꾼 실행
         h1 = Thread1(self)
         h1.start()
+
+    def a_manage(self):
+        print("계좌 관리")
+        h2 = Thread2(self)
+        h2.start()
+
 
 if __name__=='__main__':             # import된 것들을 실행시키지 않고 __main__에서 실행하는 것만 실행 시킨다.
                                      # 즉 import된 다른 함수의 코드를 이 화면에서 실행시키지 않겠다는 의미이다.
---
+++ Qthread_2.py
@@ -0,0 +1,61 @@
+from PyQt5.QtCore import *           # eventloop/스레드를 사용 할 수 있는 함수 가져옴.
+from kiwoom import Kiwoom            # 로그인을 위한 클래스
+from PyQt5.QtWidgets import *        # PyQt import
+from PyQt5.QtTest import *           # 시간관련 함수
+from datetime import datetime, timedelta    # 특정 일자를 조회
+
+
+class Thread2(QThread):
+    def __init__(self, parent):     # 부모의 윈도우 창을 가져올 수 있다.
+        super().__init__(parent)    # 부모의 윈도우 창을 초기화 한다.
+        self.parent = parent        # 부모의 윈도우를 사용하기 위한 조건
+
+        ################## 키움서버 함수를 사용하기 위해서 kiwoom의 능력을 상속 받는다.
+        self.k = Kiwoom()
+        ################## 사용되는 변수
+        self.Find_down_Screen = "1200"  # 계좌평가잔고내역을 받기위한 스크린
+        self.code_in_all = None  # 1600개 코드 중 1개 코드, 쌓이지 않고 계속 갱신
+
+        ###### 슬롯
+        self.k.kiwoom.OnReceiveTrData.connect(self.trdata_slot)  # 내가 알고 있는 Tr 슬롯에다 특정 값을 던져 준다.
+
+        ###### EventLoop
+        self.detail_account_info_event_loop = QEventLoop()  # 계좌 이벤트루프
+
+        ###### 기관외국인 평균가 가져오기
+        self.C_K_F_class()
+
+
+
+    def C_K_F_class(self):
+
+        code_list = []
+
+        for code in self.k.acc_portfolio.keys():
+            code_list.append(code)
+
+        print("계좌 종목 개수 %s" % (code_list))
+
+        #self.parent.progressBar5.setMaximum(len(code_list) - 1) /차 후 설명 드리겠습니다.
+
+        for idx, code in enumerate(code_list):
+            #self.parent.progressBar5.setValue(idx) / 차 후 설명드리겠습니다.
+
+            QTest.qWait(1000)
+
+            self.k.kiwoom.dynamicCall("DisconnectRealData(QString)", self.Find_down_Screen)  # 해당 스크린을 끊고 다시 시작
+
+            self.code_in_all = code  # 종목코드 선언 (중간에 코드 정보 받아오기 위해서)
+            print("%s / %s : 종목 검사 중 코드이름 : %s." % (idx + 1, len(code_list), self.code_in_all))
+
+            date_today = datetime.today().strftime("%Y%m%d")
+            date_prev = datetime.today() - timedelta(10)  # 넉넉히 10일전의 데이터를 받아온다. 또는 20일이상 데이터도 필요
+            date_prev = date_prev.strftime("%Y%m%d")
+
+            self.k.kiwoom.dynamicCall("SetInputValue(QString, QString)", "종목코드", code)
+            self.k.kiwoom.dynamicCall("SetInputValue(QString, QString)", "시작일자", date_prev)
+            self.k.kiwoom.dynamicCall("SetInputValue(QString, QString)", "종료일자", date_today)
+            self.k.kiwoom.dynamicCall("SetInputValue(QString, QString)", "기관추정단가구분", "1")
+            self.k.kiwoom.dynamicCall("SetInputValue(QString, QString)", "외인추정단가구분", "1")
+            self.k.kiwoom.dynamicCall("CommRqData(String, String, int, String)", "종목별기관매매추이요청2", "opt10045", "0", self.Find_down_Screen)
+            self.detail_account_info_event_loop.exec_()
