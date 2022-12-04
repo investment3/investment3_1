ff45e1b003fa37a7a7501329a2916f74525cec93
mini <maya2001bee@naver.com>
2022-12-04 12:34:20 +0900

    실시간 자동매매 GUI

---
+++ Qthread_3.py
@@ -0,0 +1,19 @@
+import os                                   # 현재 디렉토리 확인 기능
+from PyQt5.QtCore import *                  # 쓰레드 함수를 불러온다.
+from kiwoom import Kiwoom                   # 로그인을 위한 클래스
+from kiwoomType import *
+
+
+class Thread3(QThread):
+    def __init__(self, parent):   # 부모의 윈도우 창을 가져올 수 있다.
+        super().__init__(parent)  # 부모의 윈도우 창을 초기화 한다.
+        self.parent = parent      # 부모의 윈도우를 사용하기 위한 조건
+
+        ################## 키움서버 함수를 사용하기 위해서 kiwoom의 능력을 상속 받는다.
+        self.k = Kiwoom()
+        ##################
+
+        ################## 사용되는 변수
+        account = self.parent.accComboBox.currentText()  # 콤보박스 안에서 가져오는 부분
+        self.account_num = account
+        # 계좌번호 가져오는 부분은 Qthread_3 분리 시 로그인 후 계좌번호를 가져오는 함수로 교체된다.
